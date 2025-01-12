75a9416e098fa49dc77c1cdc5ef0afa95619894a
hacker-wipr <christopher.ballenger.wipr@statefarm.com>
2025-01-12 05:31:49 -0500

    Added ability to scroll on the spedometer to change the power profiles.

--- extension.js
+++ extension.js
@@ -5,13 +5,62 @@
 
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
-
+import Clutter from 'gi://Clutter';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import {SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
 
 
 const PowerProfileIndicator = GObject.registerClass(
 class PowerProfileIndicator extends SystemIndicator {
+    powerProfiles = ['power-saver', 'balanced', 'performance'];
+    counter = 0; //To prevent overscrolling
+    ignoreUp = false; //To prevent locking the main thread
+    ingoreDown = false;
+    
+    _handleScrollEvent(event) {
+        switch (event.get_scroll_direction()) {
+            case Clutter.ScrollDirection.UP:
+                if (this.counter < 0) 
+                    this.counter = 0;
+                else 
+                    this.counter++;
+                if (this.counter > 3 && !this.ignoreUp) { //When the counter reaches 5 and power-saver hasn't been activated yet, 
+                    //Check the current power saver mode
+                    let currentProfileUp = new TextDecoder().decode(GLib.spawn_command_line_sync('powerprofilesctl get')[1]).trim();
+                    //Check for the next one
+                    let newPowerProfileUp = this.powerProfiles[this.powerProfiles.indexOf(currentProfileUp) - 1];
+                    //If power-saver isn't on, this should execute since there is a lower mode
+                    if (newPowerProfileUp) {
+                        GLib.spawn_command_line_async(`powerprofilesctl set ${newPowerProfileUp}`);
+                        this.counter = 0;
+                        this.ingoreDown = false;
+                    } else {
+                        //There are no lower power modes
+                        this.ignoreUp = true;
+                    }
+                }
+                break;
+
+            case Clutter.ScrollDirection.DOWN:
+                if (this.counter > 0) 
+                    this.counter = 0;
+                else 
+                    this.counter--;
+                if (this.counter < -3 && !this.ingoreDown) {
+                    let currentProfileDown = new TextDecoder().decode(GLib.spawn_command_line_sync('powerprofilesctl get')[1]).trim();
+                    let newPowerProfileDown = this.powerProfiles[this.powerProfiles.indexOf(currentProfileDown) + 1];
+                    if (newPowerProfileDown) {
+                        GLib.spawn_command_line_async(`powerprofilesctl set ${newPowerProfileDown}`);
+                        this.counter = 0;
+                        this.ignoreUp = false;
+                    } else {
+                        this.ingoreDown = true;
+                    }
+                }
+                break;
+        }
+    }
+    
     _init() {
         super._init();
 
@@ -20,6 +69,7 @@ class PowerProfileIndicator extends SystemIndicator {
         this._timeout = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
             this._power_profile_toggle = Main.panel.statusArea.quickSettings._powerProfiles.quickSettingsItems[0];
             if (this._power_profile_toggle) {
+                this.connect('scroll-event', (actor, event) => {this._handleScrollEvent(event)});
                 this._power_profile_toggle._proxy.connectObject('g-properties-changed', this._set_icon.bind(this), this);
                 this._set_icon();
             }
@@ -46,8 +96,10 @@ class PowerProfileIndicator extends SystemIndicator {
             this._timeout = null;
         }
 
-        this._power_profile_toggle._proxy.disconnectObject(this);
-        this._power_profile_toggle = null;
+        if (this._power_profile_toggle) {
+            this._power_profile_toggle._proxy.disconnectObject(this);
+            this._power_profile_toggle = null;
+        }
 
         this._indicator.destroy();
         this._indicator = null;
